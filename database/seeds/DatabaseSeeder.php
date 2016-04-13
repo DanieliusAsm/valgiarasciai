66157d9b8f43ae9a910ab8b99646bec84789adc6
Vilius Virsilas <vilius.virsilas@gmail.com>
2016-04-13 20:11:10 +0300

    Admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(AdminSeeder::class);
+    }
+}
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'email'=>'demo@gmail.com',
+            'username'=>'demo',
+            'password'=>bcrypt(123)
+            ]);
+
+        $this->command->info('Admin added!');
     }
 }
