b6fb89da1203fd8225c78d12d625f3a2be40225f
Vilius Virsilas <vilius.virsilas@gmail.com>
2016-04-23 18:23:41 +0300

    AdminTableSeeder

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'email'=>'demo@gmail.com',
+            'username'=>'demo',
+            'password'=>bcrypt('demo123'),
+            'created_at'=>\Carbon\Carbon::now()
+            ]);
+
+        $this->command->info('Admin added!');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,26 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
-    }
-}
-
-class AdminSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('admins')->insert([
-            'email'=>'demo@gmail.com',
-            'username'=>'demo',
-            'password'=>bcrypt('demo123'),
-            'created_at'=>\Carbon\Carbon::now()
-            ]);
-
-        $this->command->info('Admin added!');
+        $this->call(AdminTableSeeder::class);
     }
 }
