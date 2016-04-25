f85221957e156f872f0bd7c0ac7bc03580ea31bc
Vilius Virsilas <vilius.virsilas@gmail.com>
2016-04-25 22:21:44 +0300

    Some changes, sass

--- resources/views/parent.blade.php
+++ resources/views/parent.blade.php
@@ -7,11 +7,8 @@
 		
 		<title>{{ $meta_title or 'Pagrindinis' }}</title>
 		
-		<!-- Bootstrap styles -->
-		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-
-		<!-- Own styles -->
-		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+		<!-- Own, Bootstrap styles -->
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
