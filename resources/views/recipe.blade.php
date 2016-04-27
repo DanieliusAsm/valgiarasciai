d01df2f14df49e92ec641886b144acb2ad9f57d1
Marius <mariusssxxl@gmail.com>
2016-04-27 13:07:40 +0300

    bootstrap

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -1,11 +1,19 @@
 @extends('parent',['meta_title'=>'recipe'] )
 @section('content')
-    <form action="{{ url('/recipe/'.$id.'/add') }}" method="post" enctype="multipart/form-data">
-        Receptas: <textarea rows="4" cols="50" name="recipe"></textarea><br>
-        Nuotruka:
-        <input type="file" name="file" id="file">
-        <input type="hidden" value="{{ csrf_token() }}" name="_token">
-        <input type="submit" value="Patvirtinti">
-    </form>
-
+    <div class="row">
+    <div class="col-sm-4">
+        <form action="{{ url('/recipe/'.$id.'/add') }}" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="Nuotrauka">Receptas:</label>
+                <textarea class="form-control" id="Nuotrauka" rows="4" name="recipe"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="Nuotrauka">Nuotrauka:</label>
+                <input class="form-control" id="Nuotrauka" type="file" name="file" id="file">
+            </div>
+            <input type="hidden" value="{{ csrf_token() }}" name="_token">
+            <button type="submit" class="btn btn-primary">Patvirtinti</button>
+        </form>
+    </div>
+    </div>
 @stop
