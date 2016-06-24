c9c033335d0f14b4956ca80d4741b836193697ec
Danielius <d.asmontas@outlook.com>
2016-06-24 23:27:12 +0300

    create diet Frontend

--- public/js/angular/DietController.js
+++ public/js/angular/DietController.js
@@ -1,31 +1,45 @@
-diet.controller('DietController', function($scope){
-    $scope.proteins = Array('');
-    $scope.produktas = [];
+diet.controller('DietController', function ($scope) {
+    $scope.diet = [{"type": "", "time": "", "rows": []},{"type": "", "time": "", "rows": []},{"type": "", "time": "", "rows": []},{"type": "", "time": "", "rows": []},{"type": "", "time": "", "rows": []},{"type": "", "time": "", "rows": []}];
+    $scope.sumValues = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0],];
 
-    $scope.calculateValue = function(quantity, baseValue) {
-        if (quantity && baseValue){
+    $scope.calculateValue = function (quantity, baseValue) {
+        if (quantity && baseValue) {
             var amount = Number(quantity) * Number(baseValue) / 100;
-
-            //if(dataType=='protein'){
-               // $scope.proteins[parentIndex].push(5);
-           // }
-
-            return amount;
+            $scope.updateTotal();
+            return amount.toFixed(2);
         }
         return 0;
     };
 
-    $scope.getProteinSum = function(parentIndex){
-        console.log($scope.proteins);
-        var sum = 0;
-        for(var i =0; i<$scope.proteins.length; i++){
-            if(parentIndex==i){
-                for(var b=0; b<$scope.proteins[i].length;b++){
-                    sum += $scope.proteins[i][b];
-                }
-            }
+    $scope.updateTotal = function () {
+        var sumB = 0;
+        var sumR = 0;
+        var sumA = 0;
+        var sumCh = 0;
+        var sumE = 0;
 
+        for (var i = 0; i < $scope.diet.length; i++) {
+            if (typeof $scope.diet[i].rows != 'undefined') {
+                for (var b = 0; b < $scope.diet[i].rows.length; b++) {
+                    if(Number($scope.diet[i].rows[b].quantity) > 0){
+                    sumB += Number($scope.diet[i].rows[b].quantity) * Number($scope.diet[i].rows[b].baltymai) / 100;
+                    sumR += Number($scope.diet[i].rows[b].quantity) * Number($scope.diet[i].rows[b].riebalai) / 100;
+                    sumA += Number($scope.diet[i].rows[b].quantity) * Number($scope.diet[i].rows[b].angliavandeniai) / 100;
+                    sumCh += Number($scope.diet[i].rows[b].quantity) * Number($scope.diet[i].rows[b].cholesterolis) / 100;
+                    sumE += Number($scope.diet[i].rows[b].quantity) * Number($scope.diet[i].rows[b].eVerte) / 100;
+                }}
+            }
+            $scope.sumValues[i] = [sumB.toFixed(2), sumR.toFixed(2), sumA.toFixed(2), sumCh.toFixed(2), sumE.toFixed(2)];
+            sumB = 0;
+            sumR = 0;
+            sumA = 0;
+            sumCh = 0;
+            sumE = 0;
         }
-        return sum;
     }
-});
+// valueIndex 0-4 baltymai - eVerte
+    $scope.getTotal = function (dietIndex, valueIndex) {
+        return $scope.sumValues[dietIndex][valueIndex];
+    }
+})
+;
--- resources/views/includes/dietform.blade.php
+++ resources/views/includes/dietform.blade.php
@@ -1,8 +1,8 @@
 <div class="form" ng-init="eatingTypes = {{$eatingTypes}};eatingTimes={{$eatingTimes}}; rows = [[{}],[{}],[{}],[{}],[{}],[{}]];"></div>
 <div class="row" ng-repeat="eatingType in eatingTypes">
-    <div class="col-md-12">
+    <div class="col-md-12" ng-init="diet[$index].type = eatingType; diet[$index].time=eatingTimes[$index]">
         <h4 ng-bind="eatingTypes[$index]"></h4>
-        <table class="table table-bordered table-hover" id="diet-table" ng-init="">
+        <table class="table table-bordered table-hover" id="diet-table">
             <col class="col-sm-1">
             <col class="col-sm-3">
             <col class="col-sm-1">
@@ -27,39 +27,39 @@
             <tr ng-repeat="row in rows[$index]">
                 <td><p ng-if="$first" ng-bind="eatingTimes[$parent.$parent.$index]"></p></td>
                 <td>
-                    <input class="form-control" type="text" ng-model="produktas[$index].product"
+                    <input class="form-control" type="text" ng-model="diet[$parent.$index].rows[$index]"
                            uib-typeahead="product as product.pavadinimas for product in products | filter:$viewValue | limitTo:10">
                 </td>
                 <td>
-                    <input class="form-control" ng-model="produktas[$index].quantity" id="quantity" type="number" name="quantity[]"
+                    <input class="form-control" ng-model="diet[$parent.$index].rows[$index].quantity" id="quantity" type="number" name="quantity[]"
                            placeholder="100" min="0"/>
                 </td>
-                <td><input class="form-control" ng-model="produktas[$index].baltymai" id="disabledInput" name="baltymai[]" placeholder="0"
-                           ng-value="calculateValue(produktas[$index].quantity,produktas[$index].product.baltymai)"
+                <td><input class="form-control" id="disabledInput" name="baltymai[]" placeholder="0"
+                           ng-value="calculateValue(diet[$parent.$index].rows[$index].quantity,diet[$parent.$index].rows[$index].baltymai)"
                            disabled/></td>
                 <td><input class="form-control" id="disabledInput" type="number" name="riebalai" placeholder="0"
-                           ng-value="calculateValue(produktas[$index].quantity,produktas[$index].product.riebalai)"
+                           ng-value="calculateValue(diet[$parent.$index].rows[$index].quantity,diet[$parent.$index].rows[$index].riebalai)"
                            disabled/></td>
                 <td><input class="form-control" id="disabledInput" type="number" name="angliavandeniai" placeholder="0"
-                           ng-value="calculateValue(produktas[$index].quantity,produktas[$index].product.angliavandeniai)"
+                           ng-value="calculateValue(diet[$parent.$index].rows[$index].quantity,diet[$parent.$index].rows[$index].angliavandeniai)"
                            disabled/></td>
                 <td><input class="form-control" id="disabledInput" type="number" name="eVerte" placeholder="0"
-                           ng-value="calculateValue(produktas[$index].quantity,produktas[$index].product.eVerte)"
+                           ng-value="calculateValue(diet[$parent.$index].rows[$index].quantity,diet[$parent.$index].rows[$index].eVerte)"
                            disabled/></td>
                 <td><input class="form-control" id="disabledInput" type="number" name="cholesterolis" placeholder="0"
-                           ng-value="calculateValue(produktas[$index].quantity,produktas[$index].product.cholesterolis)"
+                           ng-value="calculateValue(diet[$parent.$index].rows[$index].quantity,diet[$parent.$index].rows[$index].cholesterolis)"
                            disabled/></td>
-                <td><@ produktas @></td>
+                <td><@ diet @></td>
             </tr>
             </tbody>
             <tfoot>
             <tr>
                 <th class="text-center" colspan="3">Bendra pusryčių maistinė ir energinė vertė</th>
-                <th></th>
-                <th>0</th>
-                <th>0</th>
-                <th>0</th>
-                <th>0</th>
+                <th><@ sumValues[$index][0] @></th>
+                <th><@ sumValues[$index][1] @></th>
+                <th><@ sumValues[$index][2] @></th>
+                <th><@ sumValues[$index][4] @></th>
+                <th><@ sumValues[$index][3] @></th>
             </tr>
             </tfoot>
         </table>
